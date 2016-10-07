b547761a847c23a36fc83821cdbd61a73f254e14
Luiz Picolo <luizpicolo@gmail.com>
2016-10-06 22:27:33 -0400

    Adicionado configuracoes para rvm; Corrigido padrão de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :vendas do
-    resources :produtos
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :vendas
+  resources :produtos
 end
